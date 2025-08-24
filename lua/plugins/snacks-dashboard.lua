278f3b6f3057a61b1851844a7d8893e7a5cb3cff
Advancering Newholder <61349455+newholder@users.noreply.github.com>
2025-08-24 14:18:22 +0800

    Fix FIle Explorer

--- lua/plugins/snacks-dashboard.lua
+++ lua/plugins/snacks-dashboard.lua
@@ -16,7 +16,20 @@ return {
         quickfile = { enabled = true },
         scope = { enabled = true },
         --scroll = { enabled = true },
-        statuscolumn = { enabled = true },
+        statuscolumn = {
+            enabled = false, -- conflict with Oil
+            -- left = { 'mark', 'sign' }, -- priority of signs on the left (high to low)
+            right = { 'fold', 'git' }, -- priority of signs on the right (high to low)
+            folds = {
+                open = true, -- show open fold icons
+                git_hl = true, -- use Git Signs hl for fold icons
+            },
+            git = {
+                -- patterns to match Git signs
+                patterns = { 'GitSign', 'MiniDiffSign' },
+            },
+            refresh = 50, -- refresh at most every 50ms
+        },
         words = { enabled = true },
         dashboard = {
             enabled = true,
